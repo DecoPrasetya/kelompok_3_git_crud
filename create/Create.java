cd3fe00f405c4e6d3e1ace230ebe39552de38c04
Cristian <cristianreynaldi2@gmail.com>
2026-05-26 21:12:45 +0700

    delete crud

--- create/Create.java
+++
@@ -1,4 +0,0 @@
-      public void addTask(Task task){
-          taskList.add(task);
-          System.out.println("Task added successfully!");
-      }
--- delete.java
+++
@@ -1,4 +0,0 @@
-      public void deleteTask(int id){
-          taskList.removeIf(t -> t.getId() == id);
-          System.out.println("Task deleted (if existed).");
-      }
